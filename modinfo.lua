c28a79f78fd0133b2f3886ba1daa19e69c07d7eb
张森 <3180387276@qq.com>
2025-12-05 14:55:48 +0800

    mod基础信息添加

---
+++ modinfo.lua
@@ -0,0 +1,24 @@
+-- 名称
+name = "本地mod测试"
+-- 描述
+description = "这是一个测试使用的mod"
+-- 作者
+author = "zs"
+-- 版本
+version = "0.1"
+-- klei官方论坛地址，为空则默认是工坊的地址
+forumthread = ""
+-- modicon 下一篇再介绍怎么创建的
+ icon_atlas = "images/modicon.xml"
+ icon = "images/modicon.tex"
+-- dst兼容
+dst_compatible = true
+-- 是否是客户端mod
+client_only_mod = true
+-- 是否是所有客户端都需要安装
+all_clients_require_mod = false
+-- 饥荒api版本，固定填10
+api_version = 10
+
+-- mod的配置项，后面介绍
+configuration_options = {}
